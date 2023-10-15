268c9d4b9864eb8b6906e643e521080ce1f7162c
Halyna Riabokonenko <sgallina89@gmail.com>
2023-10-15 19:41:02 +0200

    completed zadanie1 with regexp

--- zadanie1.js
+++ zadanie1.js
@@ -7,12 +7,22 @@
 const text = "Pierwszy #projekt #javascript";
 
 //z uzyciem Regex;
-const textRegex = "#w+";
-const word1 = text.match(textRegex);
-const word2 = text.match(textRegex);
-console.log(word1);
-console.log(word2);
+const textRegex = /#\w+/g;
+let wordsArr = text.match(textRegex);
+console.log(wordsArr);
+
+function removeHashFromArr(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].substring(1);
+  }
+  console.log(arr);
+  return arr;
+}
+
+let arrWithoutHash = removeHashFromArr(wordsArr);
+let strWithoutHash = arrWithoutHash.toString(" ");
+console.log(strWithoutHash);
 
 //z uzyciem .slise();
-console.log(text.slice(10, 17));
-console.log(text.slice(19, 29));
+// console.log(text.slice(10, 17));
+// console.log(text.slice(19, 29));
