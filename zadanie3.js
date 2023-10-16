304fe9977b43532eae3f37438d36bfc010f99b4a
Halyna Riabokonenko <sgallina89@gmail.com>
2023-10-16 16:21:24 +0200

    add variables and function randomChar to zadanie3

--- zadanie3.js
+++ zadanie3.js
@@ -10,3 +10,32 @@
 
 // minimalnej ilości znaków z wybranych grup
 // oczekiwanej długości hasła, w przedziale np. 12-24 znaków
+
+let minLowerLetterChar = 1;
+let minUpperLetterChar = 1;
+let minNumberChar = 1;
+let minSpecialChar = 1;
+let minPassLength = 12;
+let maxPassLength = 24;
+
+const lowerLetterCharsList = "abcdefghijklmnopqrstuvwxyz";
+const upperLetterCharsList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbersCharsList = "1234567890";
+const specialCharsList = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+let passLength = minPassLength;
+
+function randomChar(charList) {
+  let charactersLength = charList.length;
+  return charList.charAt(Math.floor(Math.random() * charactersLength));
+}
+
+let lowerLetterChar = randomChar(lowerLetterCharsList);
+let upperLetterChar = randomChar(upperLetterCharsList);
+let numbersChar = randomChar(numbersCharsList);
+let specialChar = randomChar(specialCharsList);
+
+console.log(lowerLetterChar);
+console.log(upperLetterChar);
+console.log(numbersChar);
+console.log(specialChar);
