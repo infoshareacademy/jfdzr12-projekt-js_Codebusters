50c8b7d41408a923d7f4fe0ccd84aeb10392a916
Halyna Riabokonenko <sgallina89@gmail.com>
2023-10-16 23:01:18 +0200

    delete randomChar, change randomCharsList for all chars, add verification to functions

--- zadanie3.js
+++ zadanie3.js
@@ -17,7 +17,18 @@ let minNumberChar = 1;
 let minSpecialChar = 1;
 let minPassLength = 12;
 let maxPassLength = 24;
-let passCharLength = minPassLength;
+let passCharLength =
+  Math.floor(Math.random() * (maxPassLength - minPassLength + 1)) +
+  minPassLength;
+
+let randomCharsLength =
+  passCharLength -
+  minLowerLetterChar -
+  minUpperLetterChar -
+  minNumberChar -
+  minSpecialChar;
+
+console.log(randomCharsLength);
 
 const lowerLetterCharsList = "abcdefghijklmnopqrstuvwxyz";
 const upperLetterCharsList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -26,47 +37,34 @@ const specialCharsList = "!@#$%^&*()_+[]{}|;:,.<>?";
 const allCharsList =
   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+[]{}|;:,.<>?";
 
-//Function to generate random chars from list of string
-function randomChar(charList) {
-  let charactersLength = charList.length;
-  return charList.charAt(Math.floor(Math.random() * charactersLength));
+//Function to generate random chars list from chars string
+function randomCharsList(charList, charsNumber) {
+  if (charList.length === 0) {
+    console.error("Error: charList should not be empty.");
+    return null;
+  }
+
+  const charsListLength = charList.length;
+  let charsListRandom = "";
+  for (let i = 0; i < charsNumber; i++) {
+    charsListRandom += charList[Math.floor(Math.random() * charsListLength)];
+  }
+  return charsListRandom;
 }
 
-let lowerLetterChar = randomChar(lowerLetterCharsList);
-let upperLetterChar = randomChar(upperLetterCharsList);
-let numbersChar = randomChar(numbersCharsList);
-let specialChar = randomChar(specialCharsList);
+let lowerLetterChar = randomCharsList(lowerLetterCharsList, minLowerLetterChar);
+let upperLetterChar = randomCharsList(upperLetterCharsList, minUpperLetterChar);
+let numbersChar = randomCharsList(numbersCharsList, minNumberChar);
+let specialChar = randomCharsList(specialCharsList, minSpecialChar);
+let randomChars = randomCharsList(
+  allCharsList,
+  Math.max(randomCharsLength, 0) //  Ensure non-negative length
+);
 
 console.log(lowerLetterChar);
 console.log(upperLetterChar);
 console.log(numbersChar);
 console.log(specialChar);
-
-//Function to generate another password chars
-function randomCharsList(charsNumber) {
-  let charsList = "";
-  const allCharsListLength = allCharsList.length;
-  let counter = 0;
-  while (counter < charsNumber) {
-    charsList += allCharsList.charAt(
-      Math.floor(Math.random() * allCharsListLength)
-    );
-    counter += 1;
-  }
-  return charsList;
-}
-
-//Length of another password chars
-let randomCharsLength =
-  passCharLength -
-  minLowerLetterChar -
-  minUpperLetterChar -
-  minNumberChar -
-  minSpecialChar;
-console.log(randomCharsLength);
-
-//List of another password chars
-let randomChars = randomCharsList(randomCharsLength);
 console.log(randomChars);
 
 let generatedPassword =
