988059a33ee3c38e518e7287ea824942df066a61
Halyna Riabokonenko <sgallina89@gmail.com>
2023-12-04 10:29:21 +0100

    close basket modal

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@ const products = [
   {
     id: "0",
     image: "examples_photos/family.jpg",
-    title: "Family photoshoot",
+    title: "Family photo shoot",
     tags: ["family", "love"],
     location: "studio",
     cost: "10$",
@@ -10,7 +10,7 @@ const products = [
   {
     id: "1",
     image: "examples_photos/newborn.jpg",
-    title: "Newborn photoshoot",
+    title: "Newborn photo shoot",
     tags: ["newborn", "baby"],
     location: "home, studio",
     cost: "15$",
@@ -18,7 +18,7 @@ const products = [
   {
     id: "2",
     image: "examples_photos/wedding.jpg",
-    title: "Wedding photoshoot",
+    title: "Wedding photo shoot",
     tags: ["wedding", "love"],
     location: "outdoors, studio",
     cost: "10$",
@@ -44,14 +44,14 @@ photos.forEach((photo) => {
     modal.classList.add("portfolio__modal");
     portfolioSection.prepend(modal);
 
-    // OVERLAY CREATE
+    // OVERLAY CREATE AND DISPLAY
     const overlay = document.createElement("div");
     overlay.classList.add("overlay");
     document.body.appendChild(overlay);
-
     overlay.style.display = "block";
     document.body.style.overflow = "hidden";
 
+    // IMAGE CREATE AND DISPLAY
     const imageContainer = document.createElement("div");
     imageContainer.classList.add("portfolio__image-container");
     modal.prepend(imageContainer);
@@ -64,20 +64,24 @@ photos.forEach((photo) => {
     image.src = `${foundProduct.image}`;
     imageContainer.prepend(image);
 
+    // INFO CONTAINER CREATE AND DISPLAY
     const infoContainer = document.createElement("div");
     infoContainer.classList.add("portfolio__info-container");
     modal.append(infoContainer);
 
+    // TITLE CREATE AND DISPLAY
     const title = document.createElement("h2");
     title.classList.add("portfolio__title");
     title.innerText = `${foundProduct.title}`;
     infoContainer.prepend(title);
 
+    // PRICE CREATE AND DISPLAY
     const price = document.createElement("p");
     price.classList.add("portfolio__price");
     price.innerText = `${foundProduct.cost}`;
     infoContainer.append(price);
 
+    // TAGS CREATE AND DISPLAY
     const tagsContainer = document.createElement("div");
     tagsContainer.classList.add("portfolio__tags-container");
 
@@ -89,11 +93,13 @@ photos.forEach((photo) => {
       infoContainer.append(tagsContainer);
     });
 
+    // LOCATION CREATE AND DISPLAY
     const locationText = document.createElement("p");
     locationText.classList.add("portfolio__location-text");
     locationText.innerText = `Location: ${foundProduct.location}`;
     infoContainer.append(locationText);
 
+    // BUTTON BUY CREATE AND DISPLAY
     const buttonBuy = document.createElement("button");
     buttonBuy.classList.add("portfolio__button-buy");
     buttonBuy.innerHTML = "Add to basket";
@@ -101,6 +107,7 @@ photos.forEach((photo) => {
 
     buttonBuy.addEventListener("click", addToLocalStorage);
 
+    //ADD TO LOCAL STORAGE
     function addToLocalStorage() {
       let basket = JSON.parse(localStorage.getItem("boughtProducts"));
 
@@ -112,6 +119,7 @@ photos.forEach((photo) => {
       localStorage.setItem("boughtProducts", JSON.stringify(basket));
     }
 
+    // CLOSE MODAL
     const buttonClose = document.createElement("button");
     buttonClose.innerHTML = "x";
     buttonClose.classList.add("modal__button-close");
@@ -126,6 +134,7 @@ photos.forEach((photo) => {
 
     function closeModal() {
       modal.remove();
+      // OVERLAY NONE
       overlay.style.display = "none";
       document.body.style.overflow = "auto";
     }
@@ -160,8 +169,7 @@ console.log("Add basket section");
 
 const basketSection = document.querySelector(".header__links--basket");
 
-console.log("BASKET SECTION", basketSection);
-
+// BASKET MODAL CREATE
 basketSection.addEventListener("click", (e) => {
   const basketModal = document.createElement("div");
   basketModal.classList.add("basket__modal");
@@ -193,11 +201,13 @@ basketSection.addEventListener("click", (e) => {
       basketImage.src = `${element.image}`;
       basketImageContainer.prepend(basketImage);
 
+      // TITLE CREATE AND DISPLAY
       const basketTitle = document.createElement("p");
       basketTitle.classList.add("basket__title");
       basketTitle.innerText = `${element.title}`;
       basketImageContainer.append(basketTitle);
 
+      // PRICE CREATE AND DISPLAY
       const basketPrice = document.createElement("p");
       basketPrice.classList.add("basket__price");
       basketPrice.innerText = `${element.cost}`;
@@ -205,6 +215,7 @@ basketSection.addEventListener("click", (e) => {
     });
   }
 
+  // PRICE CLOSE MODAL
   const basketButtonClose = document.createElement("button");
   basketButtonClose.innerHTML = "x";
   basketButtonClose.classList.add("price-modal__button-close");
@@ -217,12 +228,14 @@ basketSection.addEventListener("click", (e) => {
     }
   });
 
+  // OVERLAY CREATE
   const overlay = document.createElement("div");
   overlay.classList.add("overlay");
   document.body.appendChild(overlay);
 
   function basketCloseModal() {
     basketModal.remove();
+    // OVERLAY NONE
     overlay.style.display = "none";
     document.body.style.overflow = "auto";
   }
